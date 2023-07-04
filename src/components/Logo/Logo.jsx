f8795d7e34c7264e6ddf667286b6cc446227d631
alexchristison <alexchristison@gmail.com>
2023-07-04 15:43:32 -0400

    logo2

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,9 +4,6 @@ export default function Logo() {
   return (
     <div className="Logo">
         <h2>alPal</h2>
-        {/* <img src="https://i.imgur.com/3HSmFUl.jpg?1" alt="Logo" /> */}
-      {/* <div>al</div>
-      <div>Pal</div> */}
     </div>
   );
 }
